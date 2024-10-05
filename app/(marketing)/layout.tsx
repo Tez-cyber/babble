3281eaf6d1a587e22d80d5c99461c0cc3b891050
Salami Azeez <salamitopeazeez@gmail.com>
2024-10-05 19:28:46 +0100

    Create marketing route group with layout and page

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,15 @@
+
+type Props = {
+    children: React.ReactNode
+}
+export default function MarletingLayout(
+    { children }: Props
+) {
+  return (
+    <div className="min-h-screen flex flex-col">
+        <main className="flex-1 flex flex-col items-center justify-center">
+            {children}
+        </main>
+    </div>
+  )
+}
