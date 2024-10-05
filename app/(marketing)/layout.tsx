47735633164d52780394218703aa3984a3e81881
Salami Azeez <salamitopeazeez@gmail.com>
2024-10-05 19:36:59 +0100

    Created footer component and imported in layout

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,3 +1,4 @@
+import { Footer } from "./footer"
 import { Header } from "./header"
 
 type Props = {
@@ -12,6 +13,7 @@ export default function MarletingLayout(
         <main className="flex-1 flex flex-col items-center justify-center">
             {children}
         </main>
+        <Footer />
     </div>
   )
 }
