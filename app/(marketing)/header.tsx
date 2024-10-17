9bee82c3f689d17f6e99276f69ccad6ecd021275
Salami Azeez <salamitopeazeez@gmail.com>
2024-10-17 15:21:05 +0100

    Header update

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -1,9 +1,18 @@
+import { Loader } from "lucide-react"
 
 
 export const Header = () => {
   return (
     <header className="h-20 w-full border-b-2 border-slate-200 px-4">
-        Header
+      <div className="flex mx-auto items-center justify-between h-full lg:max-w-screen-lg">
+        <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+          {/* <Image src="" height={40} width={40} alt="logo" /> */}
+          <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">
+            Babble
+          </h1>
+        </div>
+        <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+      </div>
     </header>
   )
 }
